761e53f3a7a95bacd714d89ffbf188c26378939d
AnakinChen <qihuichen@sohu-inc.com>
2021-10-28 10:28:02 +0800

    【Add】Carthage

---
+++ Carthage/Build/iOS/Static/QHChatMan.framework/Headers/QHChatMan.h
@@ -0,0 +1,25 @@
+//
+//  QHChatMan.h
+//  QHChatMan
+//
+//  Created by qihuichen on 2021/10/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QHChatMan.
+FOUNDATION_EXPORT double QHChatManVersionNumber;
+
+//! Project version string for QHChatMan.
+FOUNDATION_EXPORT const unsigned char QHChatManVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QHChatMan/PublicHeader.h>
+
+#import <QHChatMan/QHChatBaseProtocol.h>
+#import <QHChatMan/QHChatBaseView.h>
+#import <QHChatMan/QHChatBaseConfig.h>
+#import <QHChatMan/QHChatBaseBuffer.h>
+#import <QHChatMan/QHChatBaseModel.h>
+#import <QHChatMan/QHChatBaseViewCell.h>
+#import <QHChatMan/QHChatBaseNewDataView.h>
+#import <QHChatMan/QHChatBaseUtil.h>
